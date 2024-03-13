4bab73c18313744e7eb623092472d1211c30d428
dennis.castro <c.alejandro17@gmail.com>
2024-03-13 12:43:38 -0600

    spinner added

--- climaapp/src/components/Spinner.jsx
+++ climaapp/src/components/Spinner.jsx
@@ -2,13 +2,15 @@ import React from "react";
 
 export default function Spinner() {
   return (
-    <div className="sk-chase">
-      <div className="sk-chase-dot"></div>
-      <div className="sk-chase-dot"></div>
-      <div className="sk-chase-dot"></div>
-      <div className="sk-chase-dot"></div>
-      <div className="sk-chase-dot"></div>
-      <div className="sk-chase-dot"></div>
+    <div className="loading">
+      <div className="sk-chase">
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+      </div>
     </div>
   );
 }
